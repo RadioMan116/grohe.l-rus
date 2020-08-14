15ba690791e065f6bcf3b3d600756706a335b7e8
RadioMan116 <noris75564@gmail.com>
2020-08-14 13:06:54 +0300

    #контент На сайтах РЭТ подсказки появляются не сразу // от Дмитрия Ивановича

--- templates/main/source/js/users/script.js
+++ templates/main/source/js/users/script.js
@@ -22,6 +22,26 @@ $(document).ready(function() {
 			}
 		};
 	}
+	tippy('.link-pop-glossary .icons-prop-item', {
+		// change these to your liking
+		arrow: true,
+		placement: 'top', // top, right, bottom, left
+		// trigger: 'click',
+		distance: 15, //px or string
+		// maxWidth: 300, //px or string
+		interactive: true,
+		// leave these as they are
+		// followCursor: true,
+		allowHTML: true,
+		theme: 'light',
+		appendTo: () => document.body,
+		// ignoreAttributes: true,
+		content(reference) {
+			const title = reference.getAttribute('title');
+			reference.removeAttribute('title');
+			return title;
+		},
+	});
 	var windowWidth2 = $(window).width();
 	if ($(".articles-main__slider").length) {
 		$(function() {
